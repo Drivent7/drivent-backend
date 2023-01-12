e5650c1dc7dec768e9b79f4fa1d34a4803e9f9ed
Weiller Carvalho <engweiller@hotmail.com>
2023-01-12 16:43:42 -0300

    feat: finished mandatory transactions, now i will focus on optional transactions

--- src/repositories/address-repository/index.ts
+++ src/repositories/address-repository/index.ts
@@ -1,24 +1,41 @@
 import { prisma } from "@/config";
-import { Address } from "@prisma/client";
+import { Address, Prisma } from "@prisma/client";
+import enrollmentRepository from "../enrollment-repository";
+import { CreateEnrollmentParams, UpdateEnrollmentParams } from "../enrollment-repository";
 
-async function upsert(enrollmentId: number, createdAddress: CreateAddressParams, updatedAddress: UpdateAddressParams) {
-  return prisma.address.upsert({
+async function transactions(
+  createdAddress: CreateAddressParams,
+  updatedAddress: UpdateAddressParams,
+  userId: number,
+  createdEnrollment: CreateEnrollmentParams,
+  updatedEnrollment: UpdateEnrollmentParams,
+) {
+  const enrollmentUpdateorCreate = prisma.enrollment.upsert({
     where: {
-      enrollmentId,
+      userId,
+    },
+    create: createdEnrollment,
+    update: updatedEnrollment,
+  });
+  const addressUpdateorCreate = prisma.address.upsert({
+    where: {
+      enrollmentId: (await enrollmentUpdateorCreate).id,
     },
     create: {
       ...createdAddress,
-      Enrollment: { connect: { id: enrollmentId } },
+      Enrollment: { connect: { id: (await enrollmentUpdateorCreate).id } },
     },
     update: updatedAddress,
   });
+  return await prisma.$transaction([enrollmentUpdateorCreate, addressUpdateorCreate], {
+    isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+  });
 }
-
 export type CreateAddressParams = Omit<Address, "id" | "createdAt" | "updatedAt" | "enrollmentId">;
 export type UpdateAddressParams = CreateAddressParams;
 
 const addressRepository = {
-  upsert,
+  transactions,
 };
 
 export default addressRepository;
